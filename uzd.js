d0a0dabde7a28acc4d658496864e53662d1afc03
Marius <marius@example.com>
2022-02-22 10:17:43 +0200

    uzd

---
+++ uzd.js
@@ -0,0 +1,16 @@
+const x1 = 50;
+const x2 = 'Sveiki';
+const x3 = 75;
+const x4 = false;
+
+//1.  ar x1 yra daugiau uz 40
+
+//2. ar x3 yra maziau uz x1
+
+//3.  ar x2 yra strting tipo?
+
+//4. ar x4 yra Number tipo
+
+//5. ar x1 yra daugiau uz 20 ir maziau uz 100
+
+//6. ar x1 arba x2 yra String tipo
