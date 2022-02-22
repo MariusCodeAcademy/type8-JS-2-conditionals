54a9ff4f75e797e62ed0785bea72bff5a4395e74
Marius <marius@example.com>
2022-02-22 11:54:51 +0200

    practice

---
+++ practice.js
@@ -0,0 +1,38 @@
+//
+// 1. Parasykite programa kuri apskaiciuoja 5 duotu skaiciu vidurki;
+
+//
+/*
+// 2 uzduotis
+// parasysim coda kad apskaiciuoti apskritimo
+// plota, perimetra, diametra pagal duota spinduli
+//S = pi * r * r (plotas) (kitas formules susirasti)
+
+
+3.
+// jus turite kambario ilgi, ploti ir auksti.
+// reikia apskaiciuoti viso kambario plota.
+
+// susikurti kintamuosius ilgis, plotis ir aukstis
+
+// atspausdinti konsolej
+// jusu kambario plotas yra xxx kai ilgis: 12 plotis: 15, aukstis:3
+
+// extra yra 2 langai 1.5 x 1.5 ir durys 0.8 x 2 reikia atimti ju plotu is bendro.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
